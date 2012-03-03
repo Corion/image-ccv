1a55974b823dc64f721ea06d2baec511b5fc16c7
Max Maischein <corion@corion.net>
2012-03-03 22:02:43 +0100

    Redo facetest.pl

--- examples/facetest.pl
+++ examples/facetest.pl
@@ -1,35 +1,52 @@
 #!perl -w
 use strict;
+use Getopt::Long;
+use Pod::Usage;
 use Imager;
 use Imager::Fill;
-use List::Util qw(max);
 use Image::CCV qw(detect_faces);
 
-#my $scene  = "images/face_IMG_0762_bw.png";
-#my $scene  = "images/2123317682_9e93436f77.png";
-#my $scene  = "images/IMG_0766-bw.png";
-my $scene = "images/IMG_0732_bw.png";
+=head1 NAME
 
-my @coords = detect_faces( $scene );
-#print "@$_\n" for @coords;
+facetest.pl - simple face detection
 
-my $out = Imager->new( file => $scene );
+=head1 SYNTAX
 
-for (@coords) {
-    my ($x,$y,$width,$height,$confidence) = @$_;
-    warn "($x,$y): ${width}x$height @ $confidence\n";
-    my $color = Imager::Color->new( (1-$confidence/100) *255, $confidence/100 *255, 0 );
-    
-    # Draw a nice box
-    $out->box(
-        color => $color,
-        xmin => $x,
-        ymin => $y,
-        xmax => $x+$width,
-        ymax => $y+$height,
-        aa => 1,
-    );
-};
+  facetest.pl filename.png
 
-$out->write( file => 'face.png' )
-    or die $out->errstr;
+=cut
+
+GetOptions(
+    'd|draw:s' => \my $draw_file,
+) or pod2usage();
+
+for my $scene (@ARGV) {
+    my @coords = detect_faces( $scene );
+
+    if( $draw_file ) {
+        my $out = Imager->new( file => $scene );
+
+        for (@coords) {
+            my ($x,$y,$width,$height,$confidence) = @$_;
+            my $color = Imager::Color->new( (1-$confidence/100) *255, $confidence/100 *255, 0 );
+            
+            # Draw a nice box
+            $out->box(
+                color => $color,
+                xmin => $x,
+                ymin => $y,
+                xmax => $x+$width,
+                ymax => $y+$height,
+                aa => 1,
+            );
+        };
+
+        $out->write( file => $draw_file )
+            or die $out->errstr;
+    } else {
+        for (@coords) {
+            my ($x,$y,$width,$height,$confidence) = @$_;
+            print "($x,$y): ${width}x$height @ $confidence\n";
+        };
+    }
+}
