3eab9c8e68ffa4598426cef77dcf20cd33f235e2
Max Maischein <corion@corion.net>
2012-03-06 13:55:20 +0100

    Allow extracting multiple faces, allow drawing boxes around detection area

--- examples/facecrop.pl
+++ examples/facecrop.pl
@@ -8,7 +8,7 @@ use Imager::Fill;
 use Image::CCV qw(detect_faces);
 
 use vars qw($VERSION);
-$VERSION = '0.02';
+$VERSION = '0.03';
 
 =head1 NAME
 
@@ -27,6 +27,9 @@ GetOptions(
     'width|w:s'  => \my $max_width,
     'height|h:s' => \my $max_height,
     'scale|s:s'  => \my $scale,
+    'largest'    => \my $only_largest,
+    'draw-box'   => \my $draw_box,
+    'verbose'    => \my $verbose,
 ) or pod2usage();
 $scale ||= 1.5; # default chosen by wild guess
 
@@ -36,38 +39,70 @@ for my $scene (@ARGV) {
         die "No face found\n";
     };
 
-    # Now, find the largest face (area) in this image
-    # We ignore the confidence value
-    my $max = $coords[0];
-    for (@coords) {
-        if( $_->[2] * $_->[3] > $max->[2] * $max->[3] ) {
-            $max = $_
-        }
+    if( $only_largest ) {
+        # Now, find the largest face (area) in this image
+        # We ignore the confidence value
+        my $max = $coords[0];
+        for (@coords) {
+            if( $_->[2] * $_->[3] > $max->[2] * $max->[3] ) {
+                $max = $_
+            }
+        };
+        @coords = ($max);
     };
     
-    if( $out_file ) {
-        # Scale the frame a bit up
-        my $w = $max->[2] * $scale;
-        my $h = $max->[3] * $scale;
-        my $l = max( 0, $max->[0] - $max->[2]*(($scale -1) / 2));
-        my $t = max( 0, $max->[1] - $max->[3]*(($scale -1) / 2) );
-        my $out = Imager->new( file => $scene )
-                        ->crop( left => $l, top => $t,
-                                width => $w, height => $h
-                              );
-        if( $max_width || $max_height ) {
-            $max_width  ||= $max_height;
-            $max_height ||= $max_width;
-            $out = $out->scale(
-                xpixels => $max_width,
-                ypixels => $max_height,
-                type => 'nonprop'
-            );
-        };
-        $out->write( file => $out_file )
-            or die $out->errstr;
-    } else {
-        my ($x,$y,$width,$height,$confidence) = @$max;
-        print "($x,$y): ${width}x$height @ $confidence\n";
+    if( $verbose ) {
+        print sprintf "%d Gesichter gefunden\n", 0+@coords;
+    };
+    
+    my $index = 1;
+    for my $face (@coords) {
+        if( $out_file ) {
+            my $out = Imager->new( file => $scene );            
+            my ($x,$y,$width,$height,$confidence) = @$face;
+            
+            if( $draw_box ) {
+                my $color = Imager::Color->new( (1-$confidence/100) *255, $confidence/100 *255, 0 );
+                
+                # Draw a nice box
+                $out->box(
+                    color => $color,
+                    xmin => $x,
+                    ymin => $y,
+                    xmax => $x+$width,
+                    ymax => $y+$height,
+                    aa => 1,
+                );
+            };
+            
+            # Scale the frame a bit up
+            my $w = $face->[2] * $scale;
+            my $h = $face->[3] * $scale;
+            my $l = max( 0, $face->[0] - $face->[2]*(($scale -1) / 2));
+            my $t = max( 0, $face->[1] - $face->[3]*(($scale -1) / 2) );
+            
+            $out = $out->crop( 
+                       left => $l, top => $t,
+                       width => $w, height => $h
+                   );
+            if( $max_width || $max_height ) {
+                $max_width  ||= $max_height;
+                $max_height ||= $max_width;
+                $out = $out->scale(
+                    xpixels => $max_width,
+                    ypixels => $max_height,
+                    type => 'nonprop'
+                );
+            };
+            
+            my $out_name = sprintf $out_file, $index++;
+            $out->write( file => $out_name )
+                or die $out->errstr;
+            print "$out_name\n";
+            
+        } else {
+            my ($x,$y,$width,$height,$confidence) = @$face;
+            print "($x,$y): ${width}x$height @ $confidence\n";
+        }
     }
 }
