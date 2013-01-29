1aed8074398225ffbedb8acdc30391857a9e6824
Max Maischein <corion@corion.net>
2013-01-29 20:43:28 +0100

    Add KISS FFT

--- ccvmain.c
+++ ccvmain.c
@@ -5,6 +5,8 @@
 #endif
 #include "ccv-src/lib/3rdparty/sha1/sha1.h"
 #include "ccv-src/lib/3rdparty/sha1/sha1.c"
+#include "ccv-src/lib/3rdparty/kissfft/kiss_fft.h"
+#include "ccv-src/lib/3rdparty/kissfft/kiss_fft.c"
 #include "ccv-src/lib/ccv.h"
 #include "ccv-src/lib/ccv_basic.c"
 #include "ccv-src/lib/ccv_algebra.c"
